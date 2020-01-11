0410909325277afffc9602344a2e6fd090f17459
harperjiang <harperjiang@tencent.com>
2020-01-11 18:34:40 +0800

    revise to single mode

--- leaf/paper_experiments/femnist.sh
+++ leaf/paper_experiments/femnist.sh
@@ -4,7 +4,7 @@ output_dir="${1:-./baseline}"
 
 split_seed="1549786796"
 sampling_seed="1549786595"
-num_rounds="200"
+num_rounds="100"
 
 fedavg_lr="0.004"
 declare -a fedavg_vals=( "3 1"
@@ -15,31 +15,31 @@ declare -a gossip_vals=( "1 1 5 0.5"
 )
 
 declare -a combo_vals=( "1 1 5 0.5"
-#         "1 2 5 0.5"
-#         "1 4 5 0.5"
-#         "1 8 5 0.5"
-#         "1 10 5 0.5"
-#         "1 8 1 0.5"
-#         "1 8 2 0.5"
-#         "1 8 4 0.5"
-#         "1 8 8 0.5"
-#         "1 8 10 0.5"
+         "1 2 5 0.5"
+         "1 4 5 0.5"
+         "1 8 5 0.5"
+         "1 10 5 0.5"
+         "1 8 1 0.5"
+         "1 8 2 0.5"
+         "1 8 4 0.5"
+         "1 8 8 0.5"
+         "1 8 10 0.5"
 )
 
 
 declare -a BAcombo_vals=( "1 1 5 0.5"
-#         "1 2 5 0.5"
-#         "1 4 5 0.5"
-#         "1 8 5 0.5"
-#         "1 10 5 0.5"
-#         "1 8 1 0.5"
-#         "1 8 2 0.5"
-#         "1 8 4 0.5"
-#         "1 8 8 0.5"
-#         "1 8 10 0.5"
-#         "1 8 5 0.2"
-#         "1 8 5 0.4"
-#         "1 8 5 0.8"
+         "1 2 5 0.5"
+         "1 4 5 0.5"
+         "1 8 5 0.5"
+         "1 10 5 0.5"
+         "1 8 1 0.5"
+         "1 8 2 0.5"
+         "1 8 4 0.5"
+         "1 8 8 0.5"
+         "1 8 10 0.5"
+         "1 8 5 0.2"
+         "1 8 5 0.4"
+         "1 8 5 0.8"
 )
 
 
@@ -72,7 +72,7 @@ function run_gossip() {
 	pushd models/
 #		python main.py -dataset 'femnist' -model 'cnn' --num-rounds ${num_rounds} --clients-per-round ${clients_per_round} --num-epochs ${num_epochs} -lr ${fedavg_lr}
 		python main.py -dataset 'femnist' -model 'cnn' -algorithm gossip --num-rounds ${num_rounds} --num-epochs \
-		${num_epochs} -lr ${fedavg_lr} --segment ${segment} --replica ${replica} --eval-every 1 -e ${e} --metrics-name "gossip_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}" > "gossip_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}.log" &
+		${num_epochs} -lr ${fedavg_lr} --segment ${segment} --replica ${replica} --eval-every 1 -e ${e} --metrics-name "gossip_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}" > "gossip_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}.log"
 	popd
 	move_data ${output_dir} "gossip_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}"
 }
@@ -101,7 +101,7 @@ function run_bacombo() {
 	pushd models/
 #		python main.py -dataset 'femnist' -model 'cnn' --num-rounds ${num_rounds} --clients-per-round ${clients_per_round} --num-epochs ${num_epochs} -lr ${fedavg_lr}
 		python main.py -dataset 'femnist' -model 'cnn' -algorithm BACombo --num-rounds ${num_rounds} --num-epochs \
-		${num_epochs} -lr ${fedavg_lr} --segment ${segment} --replica ${replica} --eval-every 1 -e ${e} --metrics-name "bacombo_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}" > "bacombo_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}.log" &
+		${num_epochs} -lr ${fedavg_lr} --segment ${segment} --replica ${replica} --eval-every 1 -e ${e} --metrics-name "bacombo_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}" > "bacombo_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}.log"
 	popd
 	move_data ${output_dir} "bacombo_s_${segment}_r_${replica}_epoch_${num_epochs}_e_${e}"
 }
@@ -114,7 +114,7 @@ function gossip() {
 	replica=`echo ${val_pair} | cut -d' ' -f3`
 	e=`echo ${val_pair} | cut -d' ' -f4`
 	echo "Running gossip experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
-	run_gossip  "${num_epochs}" "${segment}" "${replica}" "${e}" &
+	run_gossip  "${num_epochs}" "${segment}" "${replica}" "${e}" 
 }
 
 function combo() {
@@ -125,7 +125,7 @@ function combo() {
 	replica=`echo ${val_pair} | cut -d' ' -f3`
 	e=`echo ${val_pair} | cut -d' ' -f4`
 	echo "Running combo experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
-	run_combo  "${num_epochs}" "${segment}" "${replica}" "${e}" &
+	run_combo  "${num_epochs}" "${segment}" "${replica}" "${e}"
 }
 
 function BAcombo() {
@@ -136,7 +136,7 @@ function BAcombo() {
 	replica=`echo ${val_pair} | cut -d' ' -f3`
 	e=`echo ${val_pair} | cut -d' ' -f4`
 	echo "Running BAcombo experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
-	run_bacombo  "${num_epochs}" "${segment}" "${replica}" "${e}" &
+	run_bacombo  "${num_epochs}" "${segment}" "${replica}" "${e}"
 }
 #function run_minibatch() {
 #	clients_per_round="$1"
@@ -197,7 +197,7 @@ echo "Storing results in directory ${output_dir} (please invoke this script as: 
 # Run Gossip experiments
 for val_pair in "${gossip_vals[@]}"; do
 #	clients_per_round=`echo ${val_pair} | cut -d' ' -f1`$
-   gossip $val_pair &
+   gossip $val_pair
 #  echo ${val_pair}
 #	num_epochs=`echo ${val_pair} | cut -d' ' -f1`
 #  segment=`echo ${val_pair} | cut -d' ' -f2`
@@ -206,7 +206,7 @@ for val_pair in "${gossip_vals[@]}"; do
 #	echo "Running gossip experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
 #	run_gossip  "${num_epochs}" "${segment}" "${replica}" "${e}" &
 done
-wait
+#wait
 
 # Run combo experiments
 for val_pair in "${combo_vals[@]}"; do
@@ -217,9 +217,9 @@ for val_pair in "${combo_vals[@]}"; do
 #	e=`echo ${val_pair} | cut -d' ' -f4`
 #	echo "Running combo experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
 #	run_combo  "${num_epochs}" "${segment}" "${replica}" "${e}"&
-   combo $val_pair &
+   combo $val_pair
 done
-wait
+#wait
 
 # Run BAcombo experiments
 for val_pair in "${BAcombo_vals[@]}"; do
@@ -230,9 +230,9 @@ for val_pair in "${BAcombo_vals[@]}"; do
 #	e=`echo ${val_pair} | cut -d' ' -f4`
 #	echo "Running BAcombo experiment with ${num_epochs} local epochs, ${segment} segments, ${replica} replica, ${e} e. "
 #	run_combo  "${num_epochs}" "${segment}" "${replica}" "${e}"&
-    BAcombo $val_pair &
+    BAcombo $val_pair
 done
-wait
+#wait
 
 popd
 
